eea34cff12f9194df1f294d152b353a3cec44228
Eduardo Chávez <eduardochavez_99@outlook.com>
2020-10-04 23:07:29 -0500

    fix typos

--- database/add_fk.sql
+++ database/add_fk.sql
@@ -4,16 +4,16 @@ ALTER TABLE alarms
   ADD CONSTRAINT fk_alarms FOREIGN KEY(user_dni) REFERENCES users(dni);
 
 ALTER TABLE organizations
-  ADD CONSTRAINT fk_organizations FOREIGN KEY(user_dni) REFERENCES user(dni);
+  ADD CONSTRAINT fk_organizations FOREIGN KEY(user_dni) REFERENCES users(dni);
 
 ALTER TABLE events
-  ADD CONSTRAINT fk_events FOREIGN KEY(user_dni) REFERENCES user(dni);
+  ADD CONSTRAINT fk_events FOREIGN KEY(user_dni) REFERENCES users(dni);
 
 ALTER TABLE places
   ADD CONSTRAINT fk_places FOREIGN KEY(event_id) REFERENCES events(id) ON DELETE CASCADE;
 
 ALTER TABLE unsubscribed_events
-  ADD CONSTRAINT fk_unsubscribed_events FOREIGN KEY(event_id) REFERENCES event_id(id) ON DELETE CASCADE;
+  ADD CONSTRAINT fk_unsubscribed_events FOREIGN KEY(event_id) REFERENCES events(id) ON DELETE CASCADE;
 
 ALTER TABLE agreements
   ADD CONSTRAINT fk_agreements1 FOREIGN KEY(user_dni) REFERENCES users(dni);
